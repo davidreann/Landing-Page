2c68e29cadc89fa76b8735035bd14865aaaeae68
SewelCSTNDA <altsewel@gmail.com>
2025-04-12 20:44:21 +0800

    Dynamic product detail page
    Implemented javascript to load the details of different products into a singular product page

---
+++ product.js
@@ -0,0 +1,27 @@
+const products = {
+    "1": {
+      id: "1",
+      title: "Nike Cortez",
+      name: "Nike Cortez",
+      price: "₱5,495.00",
+      description: "One word: tradition. From athletics superstar to fashion phenom, the Cortez's retro appeal, sponge-soft midsole and see-saw detailing deliver decade after decade. This iteration combines genuine leather and heart details for a design that's destined to be love at first sight.",
+      image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5a8dec33-c0a3-4540-8c99-fc57f298a9b0/NIKE+CORTEZ.png"
+    },
+    "2": {
+      id: "2",
+      title: "Nike Killshot",
+      name: "Nike Killshot",
+      price: "₱5,195.00",
+      description: "Inspired by our original tennis shoe, the Killshot 2 gets updated with fresh palettes and mixed materials like soft suede and smooth leather. To prove you're on top, the gum rubber sole adds the cherry on the bottom.",
+      image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4e0f6d2a-62c3-47c0-8272-57bde95ca6c8/KILLSHOT+2+LEATHER.png"
+    },
+    "3": {
+        id: "3",
+        title: "Nike Vomero",
+        name: "Nike Vomero",
+        price: "₱9,395.00",
+        description: "Carve a new lane for yourself in the Vomero 5—your go-to for durability, depth and easy styling. The sleek design includes leather, textiles and plastic accents that nod to the Y2K aesthetic.",
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5a8dec33-c0a3-4540-8c99-fc57f298a9b0/NIKE+CORTEZ.png"
+    }
+  };
+  
